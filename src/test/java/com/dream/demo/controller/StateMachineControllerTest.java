2b8bead878401ff3fd3433d7799ff73dd529083c
Jake <spring-jake@126.com>
2019-11-10 20:58:28 +0800

    first commit

---
+++ src/test/java/com/dream/demo/controller/StateMachineControllerTest.java
@@ -0,0 +1,84 @@
+package com.dream.demo.controller;
+
+import com.dream.demo.service.UserService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StateMachineControllerTest {
+    private static Logger logger = LoggerFactory.getLogger(StateMachineControllerTest.class);
+
+    @Autowired
+    private WebApplicationContext wac;
+    private MockMvc mvc;
+    private MockHttpSession session;
+
+    @Before
+    public void setupMockMvc(){
+        mvc = MockMvcBuilders.webAppContextSetup(wac).build(); //初始化MockMvc对象
+        session = new MockHttpSession();
+    }
+    @Test
+    public void newLeave() throws Exception {
+        String result = mvc.perform(MockMvcRequestBuilders.get("/state/new")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .session(session)
+        )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.code").value("0"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.msg").value("success"))
+                .andReturn().getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+    @Test
+    public void apply() {
+    }
+
+    @Test
+    public void tlApproveYes() {
+    }
+
+    @Test
+    public void tlApproveNo() {
+    }
+
+    @Test
+    public void dmApproveYes() {
+    }
+
+    @Test
+    public void dmApproveNo() {
+    }
+
+    @Test
+    public void hrRecord() {
+    }
+
+    @Test
+    public void getState() {
+    }
+}
